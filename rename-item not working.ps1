6c8056e0a6d12b16230ac97d81ed96c118e4b684
Marc-Andre Quintal <45339322+maquintal@users.noreply.github.com>
2018-11-25 15:35:18 -0500

    ps1

---
+++ rename-item not working.ps1
@@ -0,0 +1,15 @@
+﻿  $folder = 'C:\Users\vanaq\Downloads\PS1\Pop 2018-May\'
+
+$collection = (Get-ChildItem $folder -Filter *.mp3 | Select-Object -First 1).Name
+
+
+#%{ ren -new ($_.Name + ".txt") }
+
+foreach ($item in $collection)
+{
+    $newname = $item.Substring(0, $item.lastindexof('-')) + ".mp3"
+
+    $newname
+
+    Rename-ItemProperty -Name -NewName $newname -Path 'C:\Users\vanaq\Downloads\PS1\Pop 2018-May\new'
+}
---
+++ youtubeDownload.ps1
@@ -0,0 +1,18 @@
+﻿#Set-ExecutionPolicy RemoteSigned -Force
+
+Set-Location "C:\Users\vanaq\Downloads\PS1"
+
+$playlisturl = Read-Host 'enter URL: '
+#$Playlisturl = "https://www.youtube.com/playlist?list=PLkHvEl7zu06o70dpsiVrRbYFLWreD9Jcw"
+$VideoUrls= (invoke-WebRequest -uri $Playlisturl).Links | ? {$_.HREF -like "/watch*"} | `
+? innerText -notmatch ".[0-9]:[0-9]." | ? {$_.innerText.Length -gt 3} | Select innerText, `
+@{Name="URL";Expression={'http://www.youtube.com' + $_.href}} | ? innerText -notlike "*Play all*"
+ 
+$VideoUrls
+ 
+ForEach ($video in $VideoUrls){
+Write-Host ("Downloading " + $video.innerText)
+.\youtube-dl.exe $video.URL
+}
+
+#to download https://www.youtube.com/watch?v=Wet5OM7RR8Q&list=PLGpxiRQ20U6NSYZNdWMzFogtFT-9lu96z
