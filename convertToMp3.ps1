f2f738d5bbe97f05f4298cf79eed3ee815c00e6e
Marc-Andre Quintal <45339322+maquintal@users.noreply.github.com>
2018-11-25 15:26:25 -0500

    Add files via upload

---
+++ convertToMp3.ps1
@@ -0,0 +1,22 @@
+﻿   $outputExtension = ".mp3"
+   $bitrate = 160
+   $channels = 2
+
+   $folder = 'C:\Users\vanaq\Downloads\PS1\Spice Girls'
+   
+   foreach($inputFile in get-childitem $folder -recurse -Include "*.mp4","*.webm")
+   { 
+     $outputFileName = [System.IO.Path]::GetFileNameWithoutExtension($inputFile.FullName) + $outputExtension;
+     $outputFileName = [System.IO.Path]::Combine($inputFile.DirectoryName, $outputFileName);
+     
+     $programFiles = ${env:ProgramFiles(x86)};
+     if($programFiles -eq $null) { $programFiles = $env:ProgramFiles; }
+     
+     $processName = $programFiles + "\VideoLAN\VLC\vlc.exe"
+     $processArgs = "-I dummy -vvv `"$($inputFile.FullName)`" --sout=#transcode{acodec=`"mp3`",ab=`"$bitrate`",`"channels=$channels`"}:standard{access=`"file`",mux=`"wav`",dst=`"$outputFileName`"} vlc://quit"
+     
+     start-process $processName $processArgs -wait
+   }
+
+  #get-childitem $folder -recurse -Include "*.mp4","*.webm" | Remove-Item
+   get-childitem $folder -recurse -Include "*.mp3}" | rename-item -newname { [io.path]::ChangeExtension($_.name, "mp3") }
